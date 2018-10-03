5a37d930b203b7c773c345a2512b9bd0587ff3f1
navarathan <navarathan.basa05@gmail.com>
2018-10-03 07:51:05 +0530

    created stateless compoents in app.js

--- src/App.js
+++ src/App.js
@@ -1,18 +1,45 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+
 import './App.css';
 
-class App extends Component {
+class App extends React.Component {
   render() {
     return (
-      <div className="App">
-        <h1>root element</h1>
-      
-        <h2>Welcome to my site</h2>
-        <p>This is a paragraph</p>
+      <div className="main">
+        <Header/>
+        <Content />
+        <Footer />
       </div>
     );
   }
 }
+class Header extends React.Component {
+  render() {
+    return (
+      <div className = "header">
+        <h2>Header Content</h2>
+      </div>
+    )
+  }
+}
+class Content extends React.Component {
+  render() {
+    return (
+      <div className = "content">
+        <h2>Content Section</h2>
+      </div>
+    )
+  }
+}
+class Footer extends React.Component {
+  render(){
+    return(
+      <div className="footer">
+        <h2>Footer Content</h2>
+      </div>
+    )
+  }
+}
+
 
 export default App;
