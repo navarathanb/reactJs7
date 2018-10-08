bb7153b5ca78a9f67cbac7b07b4c88f610da53e3
navarathanb <navarathan.basa05@gmail.com>
2018-10-08 08:08:40 +0530

    updated ternary operator

--- src/App.js
+++ src/App.js
@@ -4,9 +4,11 @@ import './App.css';
 
 class App extends Component {
   render() {
+    var name = "james";
     return (
       <div>
         <h2>Welcome to my site</h2>
+        <h3>{name =='james'?'Welcome James' : 'Welcome User'}</h3>
       </div>
     );
   }
